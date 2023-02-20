9b84d26df4901393d8b20ca10bd7d209f9b5060b
Ermias-TekileMarkos <ermaatekk@gmail.com>
2023-02-20 11:12:41 -0800

    commit msg

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,6 +18,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
-	
+
 	return (0);
 }
