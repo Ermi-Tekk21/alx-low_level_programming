8605e009ecddddf3d742205532b0fc609113fba3
Ermias-TekileMarkos <ermaatekk@gmail.com>
2023-02-20 10:37:48 -0800

    commiting

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print with proper grammar, but the outcome is a piece of art,, 
+ * followed by a new line.
+ *  
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
