438025ac48af208c67cf146b9772784caf9b9ab9
Ermias-TekileMarkos <ermaatekk@gmail.com>
2023-02-20 10:26:06 -0800

    commit msg!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a new line.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
