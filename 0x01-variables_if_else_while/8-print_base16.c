04517586786b8cdaa74bf0db0ffc3a7f40d06c88
Ermias-TekileMarkos <ermaatekk@gmail.com>
2023-03-02 00:11:08 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+	char alpha;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+
+	return (0);
+}
