036241f555f2bd155c2d6e7e5e0da7b5434dca9d
Ermias-TekileMarkos <ermaatekk@gmail.com>
2023-03-20 15:29:31 -0700

    0x09. C - Static libraries

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - prints some statement
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
+}
